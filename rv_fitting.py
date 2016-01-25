1a8c964db779aa46ffa5aee64995f32cfe2297ba
Oscar Barragan <oscaribv@gmail.com>
2016-01-25 14:08:02 +0100

    Now rv_fitting works in a more general way

--- rv_fitting.py
+++ rv_fitting.py
@@ -17,7 +17,7 @@ def rv_curve_k(t,k):
 
 #------------------------------------
 			
-def find_k_rv0(time,fase,err):
+def find_k_rv0(time,fase,err,tpe):
 	#How is the fit made? 
 	#http://docs.scipy.org/doc/scipy-0.16.0/reference/generated/scipy.optimize.curve_fit.html
 	popt,pcov = curve_fit(rv_curve,time,fase,sigma=err)
@@ -27,8 +27,7 @@ def find_k_rv0(time,fase,err):
 	sdrv0= np.sqrt(pcov[0][0])
 	sdk  = np.sqrt(pcov[1][1])
 
-	print ('k   = %5.5f +- %5.5f'%(rvk  , sdk  ))
-	print ('rv0 = %5.5f +- %5.5f'%(rvrv0, sdrv0))
+	print ('for %1s -> rv0 = %5.5f +/- %5.5f m/s'%(tpe,rvrv0,sdrv0))
 
 	return rvrv0
 
@@ -72,49 +71,6 @@ time,fase,err,tspe = np.loadtxt(fname,usecols=(0,1,2,3), \
 fase=fase*1000.
 err=err*1000.
 
-
-#Let us separate the data for the different telescopes
-time_F=[]
-fase_F=[]
-err_F =[]
-time_H=[]
-fase_H=[]
-err_H =[]
-time_M=[]
-fase_M=[]
-err_M =[]
-
-#Let us fill this data with the corresponding telescope
-for i in range(0,len(tspe)):
-	if ( tspe[i] == 'F' ):
-		time_F.append(time[i])
-		fase_F.append(fase[i])
-		err_F.append(err[i])
-	if ( tspe[i] == 'H' ):
-		time_H.append(time[i])
-		fase_H.append(fase[i])
-		err_H.append(err[i])
-	if ( tspe[i] == 'M' ):
-		time_M.append(time[i])
-		fase_M.append(fase[i])
-		err_M.append(err[i])
-
-#Let us find the fit for each telescope
-#In this way can take off the offset of each one
-rv0_F = find_k_rv0(time_F,fase_F,err_F)
-rv0_H = find_k_rv0(time_H,fase_H,err_H)
-rv0_M = find_k_rv0(time_M,fase_M,err_M)
-#Let us take off the offset for each telescope
-for i in range(0,len(fase_F)):
-	fase_F[i] = fase_F[i] - rv0_F
-
-for i in range(0,len(fase_H)):
-	fase_H[i] = fase_H[i] - rv0_H
-
-for i in range(0,len(fase_M)):
-	fase_M[i] = fase_M[i] - rv0_M
-
-
 #These lists have lists with data for the different telescopes
 time_all=[]
 fase_all=[]
@@ -136,9 +92,10 @@ for i in range(0,len(telescopes)):
 
 rv0_all=[None]*len(telescopes)
 
+print ("Extracting systemic velocities for the %i telescopes"%len(telescopes))
 #Find all the systematic velocities and put them in rv0_all
 for i in range(0,len(telescopes)):
-	rv0_all[i] = find_k_rv0(time_all[i],fase_all[i],errs_all[i])
+	rv0_all[i] = find_k_rv0(time_all[i],fase_all[i],errs_all[i],telescopes[i])
 
 #Let us take off the offset for each telescope
 for i in range(0,len(telescopes)):
@@ -155,11 +112,6 @@ for i in range(0,len(telescopes)):
 		mega_time.append(time_all[i][j])
 		mega_err.append(errs_all[i][j])
 
-#Let us create a giant array with the data without offset
-#mega_fase = np.concatenate((fase_F,fase_M,fase_H))
-#mega_time = np.concatenate((time_F,time_M,time_H))
-#mega_err  = np.concatenate((err_F,err_M,err_H))
-
 #It is time to fit the curve for k for all the data
 popt,pcov = curve_fit(rv_curve_k,mega_time,mega_fase,sigma=mega_err)
 
@@ -167,8 +119,18 @@ popt,pcov = curve_fit(rv_curve_k,mega_time,mega_fase,sigma=mega_err)
 k = popt[0]
 sigk = np.sqrt(pcov[0][0])
 
-#Print the result
-print ('k= %8f +- %8f' %(k,sigk))
+#Now let us calculate the plantary mass
+ecc = 0.0
+
+mstar = 1.0
+mpsin = planet_mass(mstar,k,Porb,ecc)
+mjup = 0.0009543
+mearth = 0.000003003 
+
+#Print the results
+print ('semi-amplitude k= %8f +- %8f m/s' %(k,sigk))
+
+print ('This corresponds to a planet mass of %1.4e M_j for a %2.2f solar mass star' % (mpsin/mjup, mstar))
 
 #Let us do a nice plot
 
@@ -186,12 +148,7 @@ for i in range(1,n):
 	rvx[i] = newx 
 	rvy[i] = rv_curve_k(rvx[i],k)
 
-#Let us scale the time with the Period
-p_F  = scale_period(time_F,T0,Porb)
-p_H  = scale_period(time_H,T0,Porb)
-p_M  = scale_period(time_M,T0,Porb)
 p_rv = scale_period(rvx   ,T0,Porb)
-
 p_all = [None]*len(telescopes)
 for i in range(0,len(telescopes)):
 	p_all[i] = scale_period(time_all[i],T0,Porb)
@@ -203,23 +160,8 @@ plt.ylim(1.5*min(rvy),max(rvy)*3)
 plt.plot(p_rv,rvy,'k',label=('RV fit with k=%2.2f m/s'%k ))
 
 for i in range(0,len(telescopes)):
-	plt.errorbar(p_all[i],fase_all[i],errs_all[i],label=telescopes[i])
-#plt.errorbar(p_F, fase_F, err_F, fmt='d', color='b', label='FIES')
-#plt.errorbar(p_H, fase_H, err_H, fmt='o', color='r', label='HARPS')
-#plt.errorbar(p_M, fase_M, err_M, fmt='s', color='g', label='McDonald')
+	plt.errorbar(p_all[i],fase_all[i],errs_all[i],label=telescopes[i],fmt='.')
 plt.legend()
 plt.savefig('rv_fit.png')
 plt.show()
 
-
-#Now let us calculate the plantary mass
-ecc = 0.0
-
-mpsin = planet_mass(1.0,k,Porb,ecc)
-mjupiter = 0.0009543
-mearth = 0.000003003 
-
-print 'planet mass = '
-print ('%3.5e solar masses' %mpsin)
-print ('%3.5e jupiter masses' %(mpsin/mjupiter))
-print ('%3.5e earth masses' %(mpsin/mearth))
