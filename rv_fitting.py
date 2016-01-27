b8f4a0ff1b16248736ed521e5036d02418b85441
Oscar Barragan <oscaribv@gmail.com>
2016-01-27 16:25:09 +0100

    Improved some errors in the NR method

--- rv_fitting.py
+++ rv_fitting.py
@@ -1,3 +1,6 @@
+#More information about curve_fit in the following link
+#http://docs.scipy.org/doc/scipy-0.16.0/reference/generated/scipy.optimize.curve_fit.html
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -6,53 +9,62 @@ import sys
 #-----------------------------------
 #This function find the eccentry anomaly by using the
 #Newton-Rapshon method
-def find_anomaly(man,ecc,anomaly=1.0,delta=1e-3):
-	f = anomaly - ecc * np.sin(anomaly) - man
-	df=	1 - ecc * np.cos(anomaly)
-	while ( f>delta):
-		dum = anomaly - f / df
-		anomaly = dum
-		f = anomaly - ecc * np.sin(anomaly) - man
-		df=	1 - ecc * np.cos(anomaly)
+def find_anomaly(man,ecc,delta=1.e-5):
+	anomaly = [0.0]*len(man)
+	f  = anomaly - ecc * np.sin(anomaly) - man
+	df =	   1.0 - ecc * np.cos(anomaly)
+	#print f,df
+	#sys.exit()
+	for i in range(0,len(man)):
+		while ( np.absolute(f[i]) >= delta):
+			dum = anomaly[i] - f[i] / df[i]
+			anomaly[i] = dum
+			f[i] = anomaly[i] - ecc * np.sin(anomaly[i]) - man[i]
+			df[i]=	1 - ecc * np.cos(anomaly[i])
 	return anomaly	
 #-------------------------------------
-def rv_circular(t,rv0,k0):
-	#This script calculates a fit assuming a circular orbit
-	rv = rv0 - k0 * np.sin( 2.0 * np.pi * ( t - T0 ) / Porb )
+	#This functions assumes that the orbit is circular
+def rv_circular(t,rv0,k):
+	rv = rv0 + k * np.sin( 2.0 * np.pi * ( t - T0 ) / Porb )
+	#rv = rv0 + k * np.sin( 2.0 * np.pi * ( t ) / Porb )
 	return rv
 #------------------------------------
 def rv_curve_rv0(t,rv0,k0,ecc,omega):
-	#rv = rv0 - k * np.sin( 2.0 * np.pi * ( t - T0 ) / Porb )
 	#The general equation for rv motion is
 	man = 2 * np.pi * (t - T0) / Porb
-	anomaly = man
-	#anomaly = find_anomaly(man,ecc)
-	rv = rv0 + k0 * (np.cos( anomaly + omega ) + ecc*np.cos( omega ) ) / np.sqrt(1 - ecc*ecc)
+	#man = 2 * np.pi * (t) / Porb
+	anomaly = find_anomaly(man,ecc)
+	rv = rv0 + k0 * (np.cos( anomaly + omega ) + ecc*np.cos( omega ) ) / np.sqrt(1.0 - ecc*ecc)
 	return rv
 #------------------------------------
 def rv_curve(t,k0,ecc,omega):
+	#The general equation for rv motion is
 	man = 2 * np.pi * ( t - T0 ) / Porb
-	anomaly = man
-	#anomaly = find_anomaly(man,ecc)
-	rv = + k0 * (np.cos( anomaly + omega ) + ecc*np.cos( omega ) ) / np.sqrt(1 - ecc*ecc)
+	#man = 2 * np.pi * ( t ) / Porb
+	anomaly = find_anomaly(man,ecc)
+	rv = + k0 * (np.cos( anomaly + omega ) + ecc*np.cos( omega ) )  / np.sqrt(1.0 - ecc*ecc)
 	return rv
 #------------------------------------
+#Extract systemic velocities is highly important, so let us take care with this
 def find_rv0(time,fase,err,tpe):
-	#How is the fit made? 
-	#http://docs.scipy.org/doc/scipy-0.16.0/reference/generated/scipy.optimize.curve_fit.html
-	#First let us do a fit assuming a circular orbit
-	popt,pcov = curve_fit(rv_circular,time,fase,sigma=err)
+	#Let us first fit assuming a circular orbit (to first estimate v0 and k0)
+	popt,pcov = curve_fit(rv_circular,time,fase)
+	#This results will be our first guesses for v0 and k0
 	rv0 = popt[0]
 	k0  = popt[1]
-	popt,pcov = curve_fit(rv_circular,time,fase,sigma=err,p0=[rv0,k0])
-	rv0 = popt[0]
-	k0  = popt[1]
-	#Now we have a first estimate of rv0 and k0
+	#Now let us fit again now with your guesses as input
+	#popt,pcov = curve_fit(rv_circular,time,fase,sigma=err,p0=[rv0,k0])
+	#rv0 = popt[0]
+	#k0  = popt[1]
+	#Now rv0 and kv0 are really good guesses to try to fit an eccentric orbit
 	popt,pcov = curve_fit(rv_curve_rv0,time,fase,sigma=err,p0=[rv0,k0,0,0])
+	rv0 = popt[0]
+	k0  = popt[1]
+	w   = popt[3]
+	#Now we have guesses for rv0, k0 and w, let us re do the fit, now to improve e
+	popt,pcov = curve_fit(rv_curve_rv0,time,fase,sigma=err,p0=[rv0,k0,0,w])
 	rvrv0= popt[0]
-	rvk  = popt[1]
 	sdrv0= np.sqrt(pcov[0][0])
-	sdk  = np.sqrt(pcov[1][1])
 	print ('for %1s -> rv0 = %5.5f +/- %5.5f m/s'%(tpe,rvrv0,sdrv0))
 	return rvrv0
 #-----------------------------
@@ -91,9 +103,6 @@ time,fase,err,tspe = np.loadtxt(fname,usecols=(0,1,2,3), \
   dtype={'names': ('time', 'fase', 'err','telescope'), \
 	'formats': ('float', 'float', 'float', 'S1')}, \
 	comments='#',unpack=True)
-
-
-ecc=0.0
 
 #Transform fase from km/s to m/s
 fase=fase*1000.
@@ -142,6 +151,9 @@ for i in range(0,len(telescopes)):
 
 #It is time to fit the curve for k for all the data
 popt,pcov = curve_fit(rv_curve,mega_time,mega_fase,sigma=mega_err,p0=[1,0,0])
+kg = popt[0]
+wg = popt[2]
+popt,pcov = curve_fit(rv_curve,mega_time,mega_fase,sigma=mega_err,p0=[kg,0,wg])
 
 #Let us store the values of k and its corresponding sigma
 k = popt[0]
@@ -169,32 +181,26 @@ print ('This corresponds to a planet mass of %1.4e M_j for a %2.2f solar mass st
 #Let us do a nice plot
 
 #Create the RV fitted curve
-n = 100
-rvx = [None]*n
-rvy = [None]*n
+n = 500
 xmin = T0
 xmax = T0 + Porb
 dn = (xmax - xmin) /  n  
+rvx = np.empty([n])
 rvx[0] = xmin 
-rvy[0] = rv_curve(rvx[0],k,e,w)
 for i in range(1,n):
-	newx   = rvx[i-1] + dn
-	rvx[i] = newx 
-	rvy[i] = rv_curve(rvx[i],k,e,w)
+	rvx[i] = rvx[i-1] + dn
+rvy = rv_curve(rvx,k,e,w)
 
-p_rv = scale_period(rvx   ,T0,Porb)
+p_rv = scale_period(rvx,T0,Porb)
 p_all = [None]*len(telescopes)
 for i in range(0,len(telescopes)):
 	p_all[i] = scale_period(time_all[i],T0,Porb)
-	#p_all[i] = time_all[i]
-
 
 #error bars -> http://matplotlib.org/1.2.1/examples/pylab_examples/errorbar_demo.html
 plt.xlabel("Phase")
 plt.ylabel("k (m/s)")
 plt.ylim(1.5*min(rvy),max(rvy)*3)
 plt.plot(p_rv,rvy,'k',label=('RV fit with k=%2.2f m/s'%k ))
-
 mark = ['o', 'd', '^', '<', '>', '8', 's', 'p', '*', 'h', 'H', 'D', 'v']
 for i in range(0,len(telescopes)):
 	plt.errorbar(p_all[i],fase_all[i],errs_all[i],label=telescopes[i],fmt=mark[i])
