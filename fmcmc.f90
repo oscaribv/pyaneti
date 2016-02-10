0d9ab8ed5217ea163e06918bfc00d1615dab1186
Oscar Barragan <oscaribv@gmail.com>
2016-02-10 20:42:55 +0100

    Let us start to work with fmcmc.f90

---
+++ fmcmc.f90
@@ -0,0 +1,30 @@
+!Template to write a fortran90 program
+!written by Oscar Barragán, Jan 13 2014
+
+!-------------------------------------------------------------------------
+!Name of the program
+!Here write a simple description of the program
+!Date -->
+!-------------------------------------------------------------------------
+
+program !Program name
+
+ implicit none !All the variables must be defined
+
+ !define the constants, SI, cgs, etc?
+ double precision, parameter :: c = 299792458, h = 6.63e-34, k = 1.38e-23 
+ !define the variables, SI, cgs, etc?
+ double precision :: !Here 
+ integer :: !variables to count
+
+!print and read
+ !print*,''
+ !read(*,*)
+
+ !do while ( )
+  !do magic here
+ !end do
+ !the integration is done
+ !add the constants
+
+end program !Program name
