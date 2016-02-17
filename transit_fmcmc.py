f8605a0104720162e9466ef0c08708a1b7b9ccd2
Oscar Barragan <oscaribv@gmail.com>
2016-02-17 15:58:05 +0100

    Now transit_fmcmc normalizes the transit data

--- transit_fmcmc.py
+++ transit_fmcmc.py
@@ -1,199 +1,94 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
-from scipy.stats import norm
+from scipy.stats import norm, sigmaclip
 import sys
 import frv
 
-#-----------------------------
-def scale_period(jd,T0,P):
-	x = [None]*len(jd)
-	for i in range(len(jd)):
-		x[i] = ( ( jd[i] - T0 ) % P ) /  P
-	return x
-#-----------------------------
-def planet_mass(mstar,k,P,ecc):
-	#Gravitational costant
-	Gc = 6.67408e-11 #m^3 / (kgs^2)
-	Gc = Gc * 1.989e30 # m^3 / (Msun s^2)
-	#period in seconds
-	P = P * 24. * 3600
-	mpsin = k * ( 2. * np.pi * Gc / P)**(-1./3.)  * \
-	mstar**(2./3.) 
-	return mpsin
-#-----------------------------
-
 #----- HERE THE MAIN PROGRAM STARTS -----#
 
-ylab = 'RV (km/s)'
-units_ms = False
-ktom = 1.0
-Porb = 1.0
-T0   = 0.0
-mstar= 1.0
-imcmc = 5000000
-is_circular = False
-chi_toler = 0.099
-prec = 1e-5
-tfactor = 10000
+fname = "corot_transit.tbl"
+ 
+skrow = 0
+#Read the data fid
+dummyd,dummyf,flag = np.loadtxt(fname,usecols=(2,9,10), comments='\\',unpack=True,skiprows=skrow)
 
-#Read the input_rv.py to know the 
-execfile('input_rv.py')
+#Let us take the good data with the flag
+wflux = []
+hdate = []
+for i in range(0,len(flag)):
+	if ( flag[i] == 0):
+		wflux.append(dummyf[i])
+		hdate.append(dummyd[i])
 
-#Read the data file
-time,rv,err,tspe = np.loadtxt(fname,usecols=(0,1,2,3), \
-  dtype={'names': ('time', 'rv', 'err','telescope'), \
-	'formats': ('float', 'float', 'float', 'S1')}, \
-	comments='#',unpack=True)
+errs = np.sqrt(wflux)
 
-#Transform rv from km/s to m/s
-if(units_ms):
-	ktom = 1000
-	rv=rv*ktom
-	err=err*ktom
-	ylab = 'RV (m/s)'
+ntr = 2
 
-#These lists have lists with data for the different telescopes
-time_all=[]
-rv_all=[]
-errs_all=[]
+tls = [None]*ntr 
 
-#Number of telescopes
-nt = len(telescopes)
+tls[0] = [3217,3218]
+#tls[1] = 
 
-#tspe is the data with the telescopes read from the file
-for i in range(0,nt):
-	time_dum =[]
-	rv_dum =[]
-	errs_dum =[]
-	if (len(telescopes[i]) == 0):
-		print ("There is no data for %s"% telescopes[i])
-	else:
-		for j in range(0,len(tspe)):
-			if (tspe[j] == telescopes[i]):
-				time_dum.append(time[j])
-				rv_dum.append(rv[j])
-				errs_dum.append(err[j])
-	#The *all variables are lists of lists, each list constains
-	# a list with the data of each telescope
-		time_all.append(time_dum)
-		rv_all.append(rv_dum)
-		errs_all.append(errs_dum)
+plt.xlim(tls[0])
+plt.plot(hdate,wflux)
+plt.show()
+#Let us try to fit a parabola to the curve
 
-#The mega* variables contains all telescope data
-mega_rv = []
-mega_time = []
-mega_err  = []
-tlab = []
-for i in range(0,nt): 
-	for j in range(0,len(rv_all[i])):
-		tlab.append(i)
-		mega_rv.append(rv_all[i][j])
-		mega_time.append(time_all[i][j])
-		mega_err.append(errs_all[i][j])
+def parabola(x,a,b,c):
+	y = a + b*x +c*x*x 
+	return y 
 
-def find_errb(x):
-	iout = len(x)/5 * 4
-	#Let us take only the converging part
-	xnew = x[iout:]
-	mu,std = norm.fit(xnew)
-	return mu, std 
+def normalize(x,y,err,limits):
+	dummyx  = []
+	dummyy  = []
+	dummyerr= []
+	newx = []
+	newy = []
+	newerr = []
+	for i in range(0,len(x)):
+		if ( x[i] > limits[0] and x[i] < limits[1] ):
+			dummyy.append(y[i])
+			dummyx.append(x[i])
+			dummyerr.append(err[i])
 
-#Let us initialize the MCMC Metropolis-Hasting algorithm
-#Let us try to do a guess for the init values
-k0vecmax = [None]*nt
-k0vecmin = [None]*nt
-k0 = 0.0
-for i in range(0,nt):
-	k0vecmin[i] = min(rv_all[i])
-	k0vecmax[i] = max(rv_all[i])
-	k0 = k0 + ( k0vecmax[i] - k0vecmin[i] ) / 2
+	newy = sigmaclip(dummyy,low=3.0,high=3.0)
+	newy = sigmaclip(newy[0],low=2.0,high=2.0)
+	for i in range(0,len(dummyx)):
+		if ( dummyy[i] > newy[1] and dummyy[i] < newy[2] ):
+			newx.append(dummyx[i])
+			newerr.append(dummyerr[i])
 
-k0 = k0 / nt
-v0 = np.zeros(nt)
-e0 = 0.1
-w0 = np.pi
+	#print len(x),len(newx), len(newy[0])	
+	popt, cov = curve_fit(parabola,newx,newy[0],sigma=newerr)
+	dummyx = np.array(dummyx)
+	par = parabola(dummyx,popt[0],popt[1],popt[2])
+	#plt.plot(newx,newy[0],'b',dummyx,par,'k')
+	#plt.show()
+	#print popt
 
-for i in range(0,nt):
-	v0[i] = ( k0vecmin[i] + k0vecmax[i] ) / 2.0
+	dummyy = dummyy / par
+	dummyerr = dummyerr / par
+	#plt.plot(dummyx,dummyy,'k')
+	#plt.show()
 
-frv.metropolis_hastings_rv(mega_time,mega_rv,mega_err,tlab,v0,k0,e0,w0,T0,Porb,prec,imcmc,tfactor,chi_toler,is_circular)
+	return dummyx, dummyy, dummyerr
+	
+	#Now we have  the values around the transit
+	
+xt= [None]*ntr	
+yt= [None]*ntr	
+et= [None]*ntr	
+xt[0],yt[0], et[0] = normalize(hdate,wflux,errs,tls[0])
 
-vari,chi2,chi2red,kmc, emc, wmc, t0mc, pmc = np.loadtxt('mh_rvfit.dat', comments='#',unpack=True, usecols=(0,1,2,3,4,5,6,7))
+plt.plot(xt[0],yt[0],'k')
+plt.show()
 
-vmc = [None]*nt
-for i in range(0,nt):
-	n = [8+i]
-	vmc[i] = np.loadtxt('mh_rvfit.dat', comments='#',unpack=True, usecols=(n))
+def mandel_agol():
+	#Write here the function
+	#This function has to be written in fortran	
 
-kval,sigk  = find_errb(kmc) 
-ecval,sige = find_errb(emc)
-wval,sigw  = find_errb(wmc)
-tval,sigt  = find_errb(t0mc)
-pval,sigp  = find_errb(pmc)
-
-vval = [None]*nt
-sigv = [None]*nt
-
-for i in range(0,nt):
-	vval[i], sigv[i]  = find_errb(vmc[i])
-
-#if eccentricity is negative, there is a shift in omega
-if (ecval < 0.0 ):
-	ecval = - ecval
-	wval  = wval + np.pi
-
-#Now let us calculate the plantary mass
-k = kval
-
-mpsin = planet_mass(mstar,k*ktom,Porb,ecval)
-mjup = 0.0009543
-mearth = 0.000003003 
-
-#Print the results
-print (' k = %4.4e +/- %4.4e m/s' %(k,sigk))
-print (' w = %4.4e +/- %4.4e rad' %(wval,sigw))
-print (' e = %4.4e +/- %4.4e    ' %(ecval,sige))
-print (' t0= %4.4e +/- %4.4e    ' %(tval,sigt))
-print (' P = %4.4e +/- %4.4e    ' %(pval,sigp))
-for i in range(0,nt):
-	print ('For %s, v = %4.4e +/- %4.4e m/s' %(telescopes[i],vval[i],sigv[i]))
-
-print ('Planet mass of %1.4e M_j (for a %2.2f solar mass star)' % (mpsin/mjup, mstar))
-
-#Let us do a nice plot
-
-T0 = tval
-
-#Create the RV fitted curve
-n = 5000
-xmin = T0
-xmax = T0 + Porb
-dn = (xmax - xmin) /  n  
-rvx = np.empty([n])
-rvx[0] = xmin 
-for i in range(1,n):
-	rvx[i] = rvx[i-1] + dn
-if ( is_circular ):
-	rvy = frv.rv_circular(rvx,0.0,T0,kval,Porb)
-else:
-	rvy = frv.rv_curve(rvx,0.0,T0,kval,Porb,ecval,wval)
-
-res = [None]*nt
-for i in range(0,nt):
-	if (is_circular):
-		res[i] = frv.rv_circular(time_all[i],0.0,T0,kval,Porb)
-	else:
-		res[i] = frv.rv_curve(time_all[i],0.0,T0,kval,Porb,ecval,wval)
-	rv_all[i] = rv_all[i] - vval[i]
-	res[i] = rv_all[i] - res[i]
-
-#Residuals
-
-p_rv = scale_period(rvx,T0,Porb)
-p_all = [None]*nt
-for i in range(0,nt):
-	p_all[i] = scale_period(time_all[i],T0,Porb)
+sys.exit('I am here!')
 
 plt.figure(2,figsize=(8,8))
 plt.subplot(211)
