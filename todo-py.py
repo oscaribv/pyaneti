6c31556a6f937969a98647bd6965a5dcf980edf7
Oscar Barragan <oscaribv@gmail.com>
2016-03-01 15:55:00 +0100

    Now todo-py.py is human readable

--- todo-py.py
+++ todo-py.py
@@ -1,63 +1,101 @@
+#-----------------------------------------------------------
+#											todo-py.py
+# This file contains a lot of useful of python functions.
+#					Oscar Barragán, March 1st, 2016   
+#-----------------------------------------------------------
+
+
+# Useful libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy.stats import norm
 import sys
-import pyaneti as pti
 
-#-----------------------------
+#-----------------------------------------------------------
+#scale_period -> To calculate the periodogram of a RV curve
+#input: jd -> time vector in julian date to be escaled (days)
+#				T0 -> time zero of the transit (days)
+#				P  -> planet orbital period (days)
+#output: x -> vector with the scaled values (days)
+#-----------------------------------------------------------
 def scale_period(jd,T0,P):
   x = [None]*len(jd)
   for i in range(len(jd)):
     x[i] = ( ( jd[i] - T0 ) % P ) /  P
   return x
-#-----------------------------
+
+#-----------------------------------------------------------
+#planet_mass -> gives the planet mass from RV parameters
+#input: mstar -> mass of the orbited star (solar masses)
+#				k			-> semi-amplitude of the RV (m/s)
+#				P  		-> planet orbital period (days)
+#				ecc   -> orbit eccentricity (no unit)
+#output:mpsin -> planet mass (stellar masses) times sin(i)
+#-----------------------------------------------------------
 def planet_mass(mstar,k,P,ecc):
   #Gravitational costant
   Gc = 6.67408e-11 #m^3 / (kgs^2)
   Gc = Gc * 1.989e30 # m^3 / (Msun s^2)
-  #period in seconds
-  P = P * 24. * 3600
+  P = P * 24. * 3600 # s
   mpsin = k * ( 2. * np.pi * Gc / P)**(-1./3.)  * \
   mstar**(2./3.)
   return mpsin
-#-----------------------------
-def bin_data(x,y,e,nbin):
+
+#-----------------------------------------------------------
+#bin_data - bin a vector x each nbin points
+#input: x	 		-> vector of a given len (n) 
+#				nbin  -> number of points to bin
+#output: nx		-> binned vector with len int(n/nbin)	 
+#WARNING!
+#if the len of your vector is not a multiple of nbin, you 
+#will lost the last residual points
+#-----------------------------------------------------------
+def bin_data(x,nbin):
   nx = []
-  ny = []
-  ne = []
   dx = 0.0
-  dy = 0.0
-  de = 0.0
   for i in range(0,len(x)):
     dx = dx + x[i]
-    dy = dy + y[i]
-    de = de + e[i]
     if ( (i+1)%nbin == 0 ):
       nx.append(dx/nbin)
-      ny.append(dy/nbin)
-      ne.append(de/nbin)
       dx = 0.0
-      dy = 0.0
-      de = 0.0
 
-  return nx, ny, ne
+  return nx
 #----------------------------------------------
 
-def find_nsits(x,y):
+def find_transits(x,y):
 
   newy = sigmaclip(y,low=3.0,high=3.0)
 
   transits, miny, maxy = y - newy
 
-#----------------------------------------------
-
+#-----------------------------------------------------------
+#parabola -> a parabola
+#input: x	 -> vector of x values 
+#				a  -> zero order term 
+#				b  -> first order term 
+#				c  -> second order term 
+#output: y -> your parabola vector
+#-----------------------------------------------------------
 def parabola(x,a,b,c):
   y = a + b*x +c*x*x
   return y
 
-
-def normalize(x,y,err,limits):
+#-----------------------------------------------------------
+# normalize_transit -> normalizes a planet transit curve
+#input: x	     -> vector with the temporal values
+#				y  		 -> vector with the flux values
+#				err		 -> vector with the flux error values
+#				limits -> 2d vector with the start and ending date
+#									of a planetary transit
+#output: dummyx, dummyy, dummyerr 
+#				 the normalized x, y and err vectors, respectively
+#WARNING!
+# This routine assumes that your transit depth is 
+#	above 3 sigma of the data				 
+#-----------------------------------------------------------
+def normalize_transit(x,y,err,limits):
   dummyx  = []
   dummyy  = []
   dummyerr= []
@@ -82,15 +120,22 @@ def normalize(x,y,err,limits):
   par = parabola(dummyx,popt[0],popt[1],popt[2])
   dummyy = dummyy / par
   dummyerr = dummyerr / par
+
   return dummyx, dummyy, dummyerr
 
-
-def find_errb(x,nconv):
+#-----------------------------------------------------------
+# find_vals_gauss -> Find the mean and standard deviation
+#							  of the last nconv points of a given array
+#input: x 		-> vector with a minimum size nconv
+#				nconv -> the last nconv points to be taken account
+#								 in the gaussian fit
+#output: mu -> peak position of the gaussian fit
+#				 std-> standard deviation of the gaussian fit
+#-----------------------------------------------------------
+def find_vals_gauss(x,nconv):
   #Let us take only the converging part
   iout = len(x) - nconv
   xnew = x[iout:]
   mu,std = norm.fit(xnew)
   return mu, std
-
-
 
