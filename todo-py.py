eab12aedf5dd1a80325ccd7edacb574af9bee610
Oscar Barragan <oscaribv@gmail.com>
2016-03-01 15:27:15 +0100

    All the python functions were moved from transit_fmcmc.py to todo-py.py

--- todo-py.py
+++ todo-py.py
@@ -22,4 +22,75 @@ def planet_mass(mstar,k,P,ecc):
   mstar**(2./3.)
   return mpsin
 #-----------------------------
+def bin_data(x,y,e,nbin):
+  nx = []
+  ny = []
+  ne = []
+  dx = 0.0
+  dy = 0.0
+  de = 0.0
+  for i in range(0,len(x)):
+    dx = dx + x[i]
+    dy = dy + y[i]
+    de = de + e[i]
+    if ( (i+1)%nbin == 0 ):
+      nx.append(dx/nbin)
+      ny.append(dy/nbin)
+      ne.append(de/nbin)
+      dx = 0.0
+      dy = 0.0
+      de = 0.0
+
+  return nx, ny, ne
+#----------------------------------------------
+
+def find_nsits(x,y):
+
+  newy = sigmaclip(y,low=3.0,high=3.0)
+
+  transits, miny, maxy = y - newy
+
+#----------------------------------------------
+
+def parabola(x,a,b,c):
+  y = a + b*x +c*x*x
+  return y
+
+
+def normalize(x,y,err,limits):
+  dummyx  = []
+  dummyy  = []
+  dummyerr= []
+  newx = []
+  newy = []
+  newerr = []
+  for i in range(0,len(x)):
+    if ( x[i] > limits[0] and x[i] < limits[1] ):
+      dummyy.append(y[i])
+      dummyx.append(x[i])
+      dummyerr.append(err[i])
+
+  newy = sigmaclip(dummyy,low=3.0,high=3.0)
+  newy = sigmaclip(newy[0],low=2.0,high=2.0)
+  for i in range(0,len(dummyx)):
+    if ( dummyy[i] > newy[1] and dummyy[i] < newy[2] ):
+      newx.append(dummyx[i])
+      newerr.append(dummyerr[i])
+
+  popt, cov = curve_fit(parabola,newx,newy[0],sigma=newerr)
+  dummyx = np.array(dummyx)
+  par = parabola(dummyx,popt[0],popt[1],popt[2])
+  dummyy = dummyy / par
+  dummyerr = dummyerr / par
+  return dummyx, dummyy, dummyerr
+
+
+def find_errb(x,nconv):
+  #Let us take only the converging part
+  iout = len(x) - nconv
+  xnew = x[iout:]
+  mu,std = norm.fit(xnew)
+  return mu, std
+
+
 
--- transit_fmcmc.py
+++ transit_fmcmc.py
@@ -9,10 +9,13 @@ import pyaneti as pti
 
 #----- HERE THE MAIN PROGRAM STARTS -----#
 
+#Read the file with all the python functions
+execfile('todo-py.py')
+
 fname = "corot_transit.tbl"
  
 skrow = 0
-#Read the data fid
+#Read the data file
 dummyd,dummyf,flag = np.loadtxt(fname,usecols=(2,9,10), comments='\\',unpack=True,skiprows=skrow)
 
 #Let us take the good data with the flag
@@ -24,38 +27,6 @@ for i in range(0,len(flag)):
 		nobin_hdate.append(dummyd[i])
 
 nobin_errs = np.sqrt(nobin_wflux)
-
-#----------------------------------------------
-
-def bin_data(x,y,e,nbin):
-	nx = []
-	ny = []
-	ne = []
-	dx = 0.0
-	dy = 0.0
-	de = 0.0
-	for i in range(0,len(x)):
-		dx = dx + x[i]
-		dy = dy + y[i]
-		de = de + e[i]
-		if ( (i+1)%nbin == 0 ):
-			nx.append(dx/nbin)
-			ny.append(dy/nbin)
-			ne.append(de/nbin)
-			dx = 0.0
-			dy = 0.0
-			de = 0.0
-
-	return nx, ny, ne
-#----------------------------------------------
-
-def find_transits(x,y):
-
-	newy = sigmaclip(y,low=3.0,high=3.0)
-
-	transits, miny, maxy = y - newy	
-
-#----------------------------------------------
 
 #bin the data to do fastest test
 
@@ -72,44 +43,11 @@ tls = [None]*ntr
 
 tls[0] = [3217,3218]
 tls[1] = [3232.1,3233.1] 
-
-def parabola(x,a,b,c):
-	y = a + b*x +c*x*x 
-	return y 
-
-def normalize(x,y,err,limits):
-	dummyx  = []
-	dummyy  = []
-	dummyerr= []
-	newx = []
-	newy = []
-	newerr = []
-	for i in range(0,len(x)):
-		if ( x[i] > limits[0] and x[i] < limits[1] ):
-			dummyy.append(y[i])
-			dummyx.append(x[i])
-			dummyerr.append(err[i])
-
-	newy = sigmaclip(dummyy,low=3.0,high=3.0)
-	newy = sigmaclip(newy[0],low=2.0,high=2.0)
-	for i in range(0,len(dummyx)):
-		if ( dummyy[i] > newy[1] and dummyy[i] < newy[2] ):
-			newx.append(dummyx[i])
-			newerr.append(dummyerr[i])
-
-	popt, cov = curve_fit(parabola,newx,newy[0],sigma=newerr)
-	dummyx = np.array(dummyx)
-	par = parabola(dummyx,popt[0],popt[1],popt[2])
-	dummyy = dummyy / par
-	dummyerr = dummyerr / par
-	return dummyx, dummyy, dummyerr
-	
-	#Now we have  the values around the transit
 	
 xt= [None]*ntr	
 yt= [None]*ntr	
 et= [None]*ntr	
-#
+
 xt[0],yt[0], et[0] = normalize(hdate,wflux,errs,tls[0])
 xt[1],yt[1], et[1] = normalize(hdate,wflux,errs,tls[1])
 
@@ -141,7 +79,7 @@ a = 13.0
 u1 = 0.42
 u2 = 0.25
 pz = 0.2
-prec = 5.e-5
+prec = 1.e-5
 maxi = int(1e8)
 chi2_toler = 0.3
 thin_factor = int(2e3)
@@ -162,29 +100,9 @@ what_fit = [int(fit_e),int(fit_w),int(fit_i),int(fit_a), \
 					  int(fit_u1),int(fit_u2),int(fit_pz),int(fit_t0), \
 						int(fit_P)]
 
-#pti.metropolis_hastings_tr(megax, megay,megae, T0,P,e, w, i, a, u1, u2, pz,tlimits,prec, maxi, thin_factor, chi2_toler, ics, what_fit, nconv)
+pti.metropolis_hastings_tr(megax, megay,megae, T0,P,e, w, i, a, u1, u2, pz,tlimits,prec, maxi, thin_factor, chi2_toler, ics, what_fit, nconv)
 
 vari,chi2,chi2red,eo, wo, io, ao, u1o, u2o, pzo,t0o,Po = np.loadtxt('mh_trfit.dat', comments='#',unpack=True)
-
-#t0o = [None]*ntr
-
-#for j in range(0,ntr):
-#	n = [10+j]
-#	t0o[j] = np.loadtxt('mh_trfit.dat', comments='#',unpack=True, usecols=(n))
-
-
-
-def find_errb(x,nconv):
-	#Let us take only the converging part
-	iout = len(x) - nconv
-	xnew = x[iout:]
-	mu,std = norm.fit(xnew)
-	return mu, std
-
-#t0_val = [None]*ntr
-#t0_err = [None]*ntr
-#for i in range(0,ntr):
-#	t0_val[i],t0_err[i] = find_errb(t0o[i],nconv)
 
 e_val,e_err = find_errb(eo,nconv)
 w_val,w_err = find_errb(wo,nconv)
@@ -232,8 +150,6 @@ zvec = pti.find_z(xvec,t0_val,P_val,e_val,w_val,i_val,a_val, tlimits)
 
 mud, mu0 = pti.occultquad(zvec,u1_val,u2_val,pz_val)
 
-
-
 plt.figure(2,figsize=(10,10))
 plt.subplot(211)
 plt.xlim(min(xt[0]),max(xt[0]))
@@ -241,7 +157,6 @@ plt.errorbar(megax,megay,megae,fmt='o',alpha=0.3)
 plt.plot(xvec,mud,'k',linewidth=2.0)
 plt.subplot(212)
 plt.xlim(min(xt[0]),max(xt[0]))
-#plt.plot(megax,res,'bo',alpha=0.3)
 plt.errorbar(megax,res,megae,fmt='o',alpha=0.3)
 plt.plot(megax,np.zeros(len(megax)),'k--',linewidth=2.0)
 plt.show()
