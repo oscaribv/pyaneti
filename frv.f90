22d21a68abc50442bfbae7f76183f4c52f99e553
Oscar Barragan <oscaribv@gmail.com>
2016-03-04 16:01:58 +0100

    frv-checked

--- frv.f90
+++ frv.f90
@@ -38,29 +38,28 @@ end subroutine
 !             + e * cos ( \omega ) ] $
 !  Where the parameters are the typical for a RV curve
 !------------------------------------------------------------
-subroutine rv_curve(t,rv0,t0,k,P,ec,w,rv,ts)
+subroutine rv_curve(t,rv0,t0,k,P,e,w,rv,ts)
 implicit none
 
 !In/Out variables
   integer, intent(in) :: ts
   double precision, intent(in), dimension(0:ts-1)  :: t
+  double precision, intent(in) :: rv0, t0, k, P, e, w
   double precision, intent(out), dimension(0:ts-1) :: rv
-  double precision, intent(in) :: k, rv0, t0, P, ec, w
 !Local variables
   double precision, parameter :: pi = 3.1415926535897932384626
   double precision, dimension(0:ts-1) :: ma, ta
-  double precision :: delta = 1e-7
-  integer :: imax
+  double precision :: delta = 1.e-7
+  integer :: imax = int(1e5)
 !External function
   external :: find_anomaly
 !
-  imax = int(1e7)
   !Calculate the mean anomaly from the input values
-  ma(:) = 2.* pi * ( t(:) - t0 ) / P
+  ma(:) = mod(2.* pi * ( t(:) - t0 ) / P, 2. * pi)
   !Obtain the eccentric anomaly by using find_anomaly
-  call find_anomaly(ma,ta,ec,delta,imax,ts)
+  call find_anomaly(ma,ta,e,delta,imax,ts)
 
-  rv(:) = rv0 + k * ( cos(ta(:) + w ) + ec * cos(w) )
+  rv(:) = rv0 + k * ( cos( ta(:) + w ) + e * cos(w) )
   
 end subroutine
 
@@ -74,37 +73,37 @@ end subroutine
 !             + e * cos ( \omega ) ] $
 !  Where the parameters are the typical for a RV curve
 !------------------------------------------------------------
-subroutine rv_curve_mp(t,rv0,t0,k,P,ec,w,rv,ts,np)
-implicit none
-
-!In/Out variables
-  integer, intent(in) :: ts, np
-  double precision, intent(in), dimension(0:ts-1)  :: t
-  double precision, intent(out), dimension(0:ts-1) :: rv
-  double precision, intent(in), dimension(0:np-1) :: k, t0, P, ec, w
-  !Here rv0 depends on the telescope systemic not the planets
-  double precision :: rv0
-!Local variables
-  double precision, parameter :: pi = 3.1415926535897932384626
-  double precision, dimension(0:ts-1) :: ma, ta
-  double precision :: delta = 1e-5
-  integer :: imax, i
-!External function
-  external :: find_anomaly
+!subroutine rv_curve_mp(t,rv0,t0,k,P,ec,w,rv,ts,np)
+!implicit none
 !
-  imax = int(1e5)
-  !Calculate the mean anomaly from the input values
-  rv(:) = rv0
-  do i = 0, np-1
-    ma(:) = 2.* pi * ( t(:) - t0(i) ) / P(i)
-   !Obtain the eccentric anomaly by using find_anomaly
-   call find_anomaly(ma(:),ta(:),ec(i),delta,imax,ts)
-   rv(:) = rv(:) + k(i) * ( cos(ta(:) + w(i) ) + ec(i) * cos(w(i)) )
-  end do
-  
-end subroutine
-
-!-----------------------------------------------------------
+!!In/Out variables
+!  integer, intent(in) :: ts, np
+!  double precision, intent(in), dimension(0:ts-1)  :: t
+!  double precision, intent(out), dimension(0:ts-1) :: rv
+!  double precision, intent(in), dimension(0:np-1) :: k, t0, P, ec, w
+!  !Here rv0 depends on the telescope systemic not the planets
+!  double precision :: rv0
+!!Local variables
+!!  double precision, parameter :: pi = 3.1415926535897932384626
+!  double precision, dimension(0:ts-1) :: ma, ta
+!  double precision :: delta = 1e-5
+!  integer :: imax, i
+!!External function
+!  external :: find_anomaly
+!!
+!  imax = int(1e5)
+!  !Calculate the mean anomaly from the input values
+!  rv(:) = rv0
+!  do i = 0, np-1
+!    ma(:) = mod(2.*pi*(t(:)-t0(i))/P(i),2.*pi)
+!!   !Obtain the eccentric anomaly by using find_anomaly
+!   call find_anomaly(ma(:),ta(:),ec(i),delta,imax,ts)
+!   rv(:) = rv(:) + k(i) * ( cos(ta(:) + w(i) ) + ec(i) * cos(w(i)) )
+!  end do
+!  
+!end subroutine
+!
+!!-----------------------------------------------------------
 ! This routine calculates the chi square for a RV curve
 ! given a set of xd-yd data points
 ! It takes into acount the possible difference in systematic
@@ -121,19 +120,19 @@ end subroutine
 ! chi2 -> a double precision value with the chi2 value
 !-----------------------------------------------------------
 !subroutine find_chi2_rv(xd,yd,errs,tlab,rv0,k,ec,w,t0,P,chi2,isc,datas,nt,np)
-subroutine find_chi2_rv(xd,yd,errs,tlab,params,chi2,isc,datas,nt)
+subroutine find_chi2_rv(xd,yd,errs,tlab,params,flag,chi2,isc,datas,nt)
 implicit none
 
 !In/Out variables
   integer, intent(in) :: datas, nt
   double precision, intent(in), dimension(0:datas-1)  :: xd, yd, errs
-  integer, intent(in), dimension(0:datas-1)  :: tlab
-  double precision, intent(in), dimension(0:4+nt)  :: params
-  logical, intent(in)  :: isc
+  integer, intent(in), dimension(0:datas-1) :: tlab
+  double precision, intent(in), dimension(0:4+nt) :: params
+  logical, intent(in)  :: isc, flag(0:3)
   double precision, intent(out) :: chi2
 !Local variables
-  double precision, dimension(0:nt-1)  :: rv0
   double precision :: t0, P, e, w, k
+  double precision, dimension(0:nt-1)  :: rv0
   double precision, parameter :: pi = 3.1415926535897932384626
   double precision, dimension(0:datas-1) :: model, res
   integer :: i, tel
@@ -147,7 +146,13 @@ implicit none
   k   = params(4)
   rv0 = params(5:4+nt) 
 
-  !print *, t0, P, e, w, k, rv0
+  if ( flag(0) ) P = 10**params(1)
+  if ( flag(1) ) then
+    e = params(2) * params(2) + params(3) * params(3)
+    w = atan2( params(2),params(3) ) 
+  end if
+  if ( flag(2) ) k = 10**params(4)
+  if ( flag(3) ) rv0(:) = 10**params(5:4+nt)
 
   tel = 0
   !If we want a circular fit, let us do it!
@@ -171,8 +176,6 @@ implicit none
 
   end if
 
-  !Let us calculate the residuals
-  ! chi^2 = \Sum_i ( M - O )^2 / \sigma^2
   res(:) = ( model(:) - yd(:) ) / errs(:) 
   chi2 = dot_product(res,res)
 
@@ -197,9 +200,7 @@ end subroutine
 !Output parameter:
 ! This functions outputs a file called mh_rvfit.dat
 !-----------------------------------------------------------
-!I could deal with differences in parameters by ussing vectors insead of
-!independient float parameters, PLEASE OSCAR DO THIS!
-subroutine metropolis_hastings_rv(xd,yd,errs,tlab,params,prec,maxi,thin_factor,ics,wtf,nconv,datas,nt)
+subroutine metropolis_hastings_rv(xd,yd,errs,tlab,params,prec,maxi,thin_factor,ics,wtf,flag,nconv,datas,nt)
 implicit none
 
 !In/Out variables
@@ -210,22 +211,33 @@ implicit none
   !f2py intent(in,out)  :: params
   double precision, intent(in), dimension(0:5) :: wtf
   double precision, intent(in)  :: prec
-  logical, intent(in) :: ics
+  logical, intent(in) :: ics, flag(0:3)
 !Local variables
   double precision, parameter :: pi = 3.1415926535897932384626
   double precision :: chi2_old, chi2_new, chi2_red
   double precision, dimension(0:4+nt) :: params_new
   double precision, dimension(0:nconv-1) :: chi2_vec, x_vec
   double precision  :: q, chi2_y, chi2_slope, toler_slope
+  double precision  :: esin, ecos
   integer :: j, nu, n
   logical :: get_out
-  real, dimension(0:5+nt) :: r
+  double precision, dimension(0:5+nt) :: r
 !external calls
   external :: init_random_seed, find_chi2_rv
 
 
+  if ( flag(0) ) params(1) = log10(params(1))
+  if ( flag(1) ) then
+    esin = sqrt(params(2)) * sin(params(3))
+    ecos = sqrt(params(2)) * cos(params(3))
+    params(2) = esin
+    params(3) = ecos
+  end if
+  if ( flag(2) ) params(4) = log10(params(4))
+  if ( flag(3) ) params(5:4+nt) = log10(params(5:4+nt))
+
   !Let us estimate our fist chi_2 value
-  call find_chi2_rv(xd,yd,errs,tlab,params,chi2_old,ics,datas,nt)
+  call find_chi2_rv(xd,yd,errs,tlab,params,flag,chi2_old,ics,datas,nt)
   !Calculate the degrees of freedom
   nu = datas - size(params)
   !If we are fixing a circular orbit, ec and w are not used 
@@ -245,24 +257,21 @@ implicit none
   n = 0
   get_out = .TRUE.
 
+   !Call a random seed 
+  print *, 'CREATING RANDOM SEED'
+  call init_random_seed()
 
   !The infinite cycle starts!
+  print *, 'STARTING INFINITE LOOP!'
   do while ( get_out )
     !r will contain random numbers for each variable
     !Let us add a random shift to each parameter
-   !Call a random seed 
-    call init_random_seed()
     call random_number(r)
     r(1:5+nt) = ( r(1:5+nt) - 0.5 ) * 2.0
-    params_new(0:4) = params(0:4) + r(1:5) * prec * wtf(0:4)
-    params_new(5:4+nt) = params(5:4+nt) + r(6:5+nt) * 10 * prec * wtf(5)
+    params_new(0:4)    = params(0:4)    + r(1:5)    * prec * wtf(0:4)
+    params_new(5:4+nt) = params(5:4+nt) + r(6:5+nt) * prec * wtf(5)
     !Let us calculate our new chi2
-    !print *, "new"
-    !print *, params(2)
-    !print *, params_new(2)
-    call find_chi2_rv(xd,yd,errs,tlab,params_new,chi2_new,ics,datas,nt)
-    !print *, chi2_old, chi2_new
-    !stop
+    call find_chi2_rv(xd,yd,errs,tlab,params_new,flag,chi2_new,ics,datas,nt)
     !Ratio between the models
     q = exp( ( chi2_old - chi2_new ) * 0.5  )
     !If the new model is better, let us save it
@@ -280,6 +289,7 @@ implicit none
     chi2_vec(n) = chi2_red
     x_vec(n) = n
     n = n + 1
+
     if ( n == size(chi2_vec) ) then
       call fit_a_line(x_vec,chi2_vec,chi2_y,chi2_slope,nconv)
       n = 0
@@ -290,6 +300,12 @@ implicit none
                   chi2_y
         get_out = .FALSE.
       end if
+
+      if ( j > imax ) then
+        print *, 'Maximum number of iteration reached!'
+        get_out = .FALSE.
+      end if
+
     end if
       !I checked covergence
   end if
