64f09c1eeae6ad12e1cdc3f29147a3f6aa5b4281
Oscar Barragan <oscaribv@gmail.com>
2016-09-28 21:43:55 -0500

    Now the init_random_seed routine is more general to several IOS

--- src/todo.f90
+++ src/todo.f90
@@ -12,60 +12,78 @@
 ! Taken from
 ! https://gcc.gnu.org/onlinedocs/gfortran/RANDOM_005fSEED.html#RANDOM_005fSEED
 !----------------------------------------------------------
+!subroutine init_random_seed()
+!!Modified to work with f90
+!!use iso_fortran_env, only: int64
+!implicit none
+!integer,parameter :: int32 = selected_int_kind(32)
+!integer, allocatable :: seed(:)
+!integer :: i, n, un, istat, dt(8), pid
+!integer(int32) :: t
+
+!call random_seed(size = n)
+!allocate(seed(n))
+!! First try if the OS provides a random number generator
+!open(newunit=un, file="/dev/urandom", access="stream", &
+!     form="unformatted", action="read", status="old", iostat=istat)
+!if (istat == 0) then
+!   read(un) seed
+!   close(un)
+!else
+!   ! Fallback to XOR:ing the current time and pid. The PID is
+!   ! useful in case one launches multiple instances of the same
+!   ! program in parallel.
+!   call system_clock(t)
+!   if (t == 0) then
+!      call date_and_time(values=dt)
+!      t = (dt(1) - 1970) * 365_int32 * 24 * 60 * 60 * 1000 &
+!           + dt(2) * 31_int32 * 24 * 60 * 60 * 1000 &
+!           + dt(3) * 24_int32 * 60 * 60 * 1000 &
+!           + dt(5) * 60 * 60 * 1000 &
+!           + dt(6) * 60 * 1000 + dt(7) * 1000 &
+!           + dt(8)
+!   end if
+!   pid = getpid()
+!   t = ieor(t, int(pid, kind(t)))
+!   do i = 1, n
+!      seed(i) = lcg(t)
+!   end do
+!end if
+!call random_seed(put=seed)
+!
+!contains
+!! This simple PRNG might not be good enough for real work, but is
+!! sufficient for seeding a better PRNG.
+!function lcg(s)
+!  integer :: lcg
+!  integer(int32) :: s
+!  if (s == 0) then
+!     s = 104729
+!  else
+!     s = mod(s, 4294967296_int32)
+!  end if
+!  s = mod(s * 279470273_int32, 4294967291_int32)
+!  lcg = int(mod(s, int(huge(0), int32)), kind(0))
+!end function lcg
+!end subroutine init_random_seed
+
+!http://stackoverflow.com/questions/18754438/generating-random-numbers-in-a-fortran-module
 subroutine init_random_seed()
-!Modified to work with f90
-!use iso_fortran_env, only: int64
-implicit none
-integer,parameter :: int32 = selected_int_kind(32)
-integer, allocatable :: seed(:)
-integer :: i, n, un, istat, dt(8), pid
-integer(int32) :: t
 
-call random_seed(size = n)
-allocate(seed(n))
-! First try if the OS provides a random number generator
-open(newunit=un, file="/dev/urandom", access="stream", &
-     form="unformatted", action="read", status="old", iostat=istat)
-if (istat == 0) then
-   read(un) seed
-   close(un)
-else
-   ! Fallback to XOR:ing the current time and pid. The PID is
-   ! useful in case one launches multiple instances of the same
-   ! program in parallel.
-   call system_clock(t)
-   if (t == 0) then
-      call date_and_time(values=dt)
-      t = (dt(1) - 1970) * 365_int32 * 24 * 60 * 60 * 1000 &
-           + dt(2) * 31_int32 * 24 * 60 * 60 * 1000 &
-           + dt(3) * 24_int32 * 60 * 60 * 1000 &
-           + dt(5) * 60 * 60 * 1000 &
-           + dt(6) * 60 * 1000 + dt(7) * 1000 &
-           + dt(8)
-   end if
-   pid = getpid()
-   t = ieor(t, int(pid, kind(t)))
-   do i = 1, n
-      seed(i) = lcg(t)
-   end do
-end if
-call random_seed(put=seed)
+      INTEGER :: i, n, clock
+      INTEGER, DIMENSION(:), ALLOCATABLE :: seed
 
-contains
-! This simple PRNG might not be good enough for real work, but is
-! sufficient for seeding a better PRNG.
-function lcg(s)
-  integer :: lcg
-  integer(int32) :: s
-  if (s == 0) then
-     s = 104729
-  else
-     s = mod(s, 4294967296_int32)
-  end if
-  s = mod(s * 279470273_int32, 4294967291_int32)
-  lcg = int(mod(s, int(huge(0), int32)), kind(0))
-end function lcg
-end subroutine init_random_seed
+      CALL RANDOM_SEED(size = n)
+      ALLOCATE(seed(n))
+
+      CALL SYSTEM_CLOCK(COUNT=clock)
+
+      seed = clock + 37 * (/ (i - 1, i = 1, n) /)
+      CALL RANDOM_SEED(PUT = seed)
+
+      DEALLOCATE(seed)
+
+end subroutine
 
 !------------------------------------------------------------
 !This subrouotine finds the time of periastron passage
