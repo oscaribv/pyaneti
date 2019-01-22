a1b61fee8581e0639494794ac9123951e3baf872
Oscar Barragan <oscaribv@gmail.com>
2019-01-22 15:59:16 +0000

    Added matrix algebra inside todo.f90

--- src/todo.f90
+++ src/todo.f90
@@ -11,8 +11,9 @@ module constants
 
 implicit none
 
-  double precision, parameter :: pi = 3.1415926535897d0
-  double precision, parameter :: two_pi = 2.d0*3.1415926535897932384626d0
+  double precision, parameter :: pi = acos(-1.d0)
+  double precision, parameter :: two_pi = 2.d0*acos(-1.d0)
+  double precision, parameter :: log_two_pi = log(2.d0*acos(-1.d0))
   double precision, parameter :: uno = 1.0d0
   double precision, parameter :: fmin=1.d-8
   double precision, parameter :: small = 1.d-5
@@ -20,7 +21,6 @@ implicit none
   !Physical parameters
   double precision, parameter :: S_radius_SI = 6.957d8 !R_sun
   double precision, parameter :: S_GM_SI = 1.3271244d20 ! G M_sun
-
 
 end module constants
 
@@ -472,3 +472,178 @@ implicit none
   i(:) = acos( b(:) / a(:) * ( 1.d0 + e(:) * sin(w(:)) ) / ( 1.d0 - e(:)*e(:) ) )
 
 end subroutine
+
+
+  subroutine fcdist(a,b,c,n,m)
+  implicit none
+  !
+  integer :: n,m
+  double precision, dimension(0:n-1) :: a
+  double precision, dimension(0:m-1) :: b
+  double precision, dimension(0:n-1,0:m-1) :: c
+  !
+  integer :: o
+
+  do o = 0, n - 1
+    c(o,:) = b(:) - a(o)
+  end do
+
+  end subroutine
+
+  !https://ww2.odu.edu/~agodunov/computing/programs/book2/Ch06/Inverse.f90
+  subroutine inverse(a,c,n)
+  !============================================================
+  ! Inverse matrix
+  ! Method: Based on Doolittle LU factorization for Ax=b
+  ! Alex G. December 2009
+  !-----------------------------------------------------------
+  ! input ...
+  ! a(n,n) - array of coefficients for matrix A
+  ! n      - dimension
+  ! output ...
+  ! c(n,n) - inverse matrix of A
+  ! comments ...
+  ! the original matrix a(n,n) will be destroyed
+  ! during the calculation
+!===========================================================
+  implicit none
+    integer n
+    double precision a(n,n), c(n,n)
+    double precision L(n,n), U(n,n), b(n), d(n), x(n)
+    double precision coeff
+    integer i, j, k
+
+    ! step 0: initialization for matrices L and U and b
+    ! Fortran 90/95 aloows such operations on matrices
+    L=0.0
+    U=0.0
+    b=0.0
+
+    !  step 1: forward elimination
+    do k=1, n-1
+      do i=k+1,n
+        coeff=a(i,k)/a(k,k)
+        L(i,k) = coeff
+        do j=k+1,n
+           a(i,j) = a(i,j)-coeff*a(k,j)
+        end do
+      end do
+    end do
+
+    ! Step 2: prepare L and U matrices
+    ! L matrix is a matrix of the elimination coefficient
+    ! + the diagonal elements are 1.0
+    do i=1,n
+      L(i,i) = 1.0
+    end do
+    ! U matrix is the upper triangular part of A
+    do j=1,n
+      do i=1,j
+         U(i,j) = a(i,j)
+      end do
+    end do
+
+    ! Step 3: compute columns of the inverse matrix C
+    do k=1,n
+      b(k)=1.0
+      d(1) = b(1)
+    ! Step 3a: Solve Ld=b using the forward substitution
+      do i=2,n
+        d(i)=b(i)
+        do j=1,i-1
+          d(i) = d(i) - L(i,j)*d(j)
+        end do
+      end do
+    ! Step 3b: Solve Ux=d using the back substitution
+      x(n)=d(n)/U(n,n)
+      do i = n-1,1,-1
+        x(i) = d(i)
+        do j=n,i+1,-1
+          x(i)=x(i)-U(i,j)*x(j)
+        end do
+        x(i) = x(i)/u(i,i)
+      end do
+    ! Step 3c: fill the solutions x(n) into column k of C
+      do i=1,n
+        c(i,k) = x(i)
+      end do
+      b(k)=0.0
+    end do
+  end subroutine inverse
+
+!Subroutine to find the determinant of a square matrix
+!Taken from https://github.com/ashwith/workspace/blob/master/fortran/determinant.f90
+!Modified as a subroutine
+!----------------------------------------------------------------------------------------
+!Function to find the determinant of a square matrix
+!Author : Louisda16th a.k.a Ashwith J. Rego
+!Description: The subroutine is based on two key points:
+!1] A determinant is unaltered when row operations are performed: Hence, using this principle,
+!row operations (column operations would work as well) are used
+!to convert the matrix into upper traingular form
+!2]The determinant of a triangular matrix is obtained by finding the product of the diagonal elements
+!
+  subroutine finddet(a,det,n)
+  IMPLICIT NONE
+  INTEGER, INTENT(IN) :: n
+  double precision, DIMENSION(n,n), intent(in) :: a
+  double precision, intent (out) :: det
+  !
+  double precision :: m, temp
+  double precision, DIMENSION(n,n) :: matrix
+  INTEGER :: i, j, k, l
+  LOGICAL :: DetExists = .TRUE.
+  matrix = a
+  l = 1
+  !Convert to upper triangular form
+  DO k = 1, n-1
+    IF (matrix(k,k) == 0) THEN
+      DetExists = .FALSE.
+      DO i = k+1, n
+        IF (matrix(i,k) /= 0) THEN
+          DO j = 1, n
+            temp = matrix(i,j)
+            matrix(i,j)= matrix(k,j)
+            matrix(k,j) = temp
+          END DO
+          DetExists = .TRUE.
+          l=-l
+          EXIT
+        ENDIF
+      END DO
+      IF (DetExists .EQV. .FALSE.) THEN
+        det = 0
+        return
+      END IF
+    ENDIF
+    DO j = k+1, n
+      m = matrix(j,k)/matrix(k,k)
+      DO i = k+1, n
+        matrix(j,i) = matrix(j,i) - m*matrix(k,i)
+      END DO
+    END DO
+  END DO
+
+  !Calculate determinant by finding product of diagonal elements
+  det = l
+  DO i = 1, n
+    det = det * matrix(i,i)
+  END DO
+
+  END subroutine finddet
+
+  subroutine fill_diag(v,M,n)
+  implicit none
+  !
+  integer, intent(in) :: n
+  double precision, intent(in) :: v(0:n-1)
+  double precision, intent(out) :: M(0:n-1,0:n-1)
+  !
+  integer :: i
+
+  M = 0.d0
+  do i = 0, n - 1
+    M(i,i) = v(i)
+  end do
+
+  end subroutine fill_diag
